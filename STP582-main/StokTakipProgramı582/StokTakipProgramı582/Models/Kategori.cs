adfff4211c6925652177d9fa3903ef520958df5f
nuyaneyup <profilo12beyup@gmail.com>
2024-12-05 11:39:53 +0300

    Add files via upload
    582 Urunler PART 1

---
+++ STP582-main/StokTakipProgramı582/StokTakipProgramı582/Models/Kategori.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakipProgramı582.Models
+{
+    public class Kategori
+    {
+        [Key]
+        public int Id { get; set; }
+        public string KategoriAdi { get; set; }
+
+        //Bir kategorinin birden fazla ürünü olabilir
+        public virtual ICollection<Urun> Urunler { get; set; } = new HashSet<Urun>();
+    }
+}
---
+++ STP582-main/StokTakipProgramı582/StokTakipProgramı582/Models/Urun.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakipProgramı582.Models
+{
+    public class Urun
+    {
+        [Key]
+        public int UrunKodu { get; set; }
+        public string UrunAdi{ get; set; }
+
+        public int KategoriId { get; set; }
+        public int TedarikciId { get; set; }
+        public int StokAdedi { get; set; }
+        public Decimal  Fiyat { get; set; }
+        public string aciklama { get; set; }
+
+        //Bir kategorinin birden fazla ürünü olabilir
+        public virtual ICollection<Urun> Urunler { get; set; } = new HashSet<Urun>();
+    }
+}
